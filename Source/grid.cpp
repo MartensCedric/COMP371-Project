c3a91bde76bc547e14c1018e3f0ea0c159d28a2b
PeterGhimself <petergranitski@gmail.com>
2020-07-05 23:18:30 -0400

    Attempt fix at Windows include statement crashes

--- Source/grid.cpp
+++ Source/grid.cpp
@@ -1,12 +1,23 @@
+#ifdef __unix__                    /* __unix__ is usually defined by compilers targeting Unix systems */
+
+#define OS_Windows 0
+#include "includes/shader.hpp"
+
+#elif defined(_WIN32) || defined(WIN32)     /* _Win32 is usually defined by compilers targeting 32 or   64 bit Windows systems */
+
+#define OS_Windows 1
 #define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
+#include "../Source/includes/shader.hpp"
+
+#endif
+
 #include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
 
 #include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
-                        // initializing OpenGL and binding inputs
+// initializing OpenGL and binding inputs
 
 #include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
-
-#include "includes/shader.hpp"
+#include <glm/gtc/matrix_transform.hpp> // needed for transformation of matrices
 
 int createCubeGrid()
 {
