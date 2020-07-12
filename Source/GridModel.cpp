bfa5ad9bd44c3d8615e8a02c2fb82cfbf5e9ffd0
Cedric Martens <cedricmartens98@gmail.com>
2020-07-12 14:20:09 -0400

    Added second I3

--- Source/GridModel.cpp
+++ Source/GridModel.cpp
@@ -5,11 +5,11 @@ GridModel::GridModel() {
     glm::vec3 color = glm::vec3(0.52f, 0.58f, 0.18f);
     
     glm::vec3 verticesGridUnit[] = {
-		glm::vec3(-50.0f, 0, 0), color,
-        glm::vec3(50.0f, 0, 0), color,
+		glm::vec3(-25.f, 0, 0), color,
+        glm::vec3(25.0f, 0, 0), color,
 
-        glm::vec3(0, 0, -50.0f), color,
-        glm::vec3(0, 0, 50.0f), color,
+        glm::vec3(0, 0, -25.f), color,
+        glm::vec3(0, 0, 25.f), color,
     };
 
 	for (int i = 0; i < 100; i++)
@@ -20,7 +20,7 @@ GridModel::GridModel() {
 			glDrawArrays(GL_LINES, 0, 2);
 		});
 
-		xline->translate(0, 0, -50.0f + i);
+		xline->translate(0, 0, -25.0f + i/2.0f);
 		xline->setupAttribPointer();
 
 		SimpleModel* zline = new SimpleModel(verticesGridUnit, sizeof(verticesGridUnit) / sizeof(verticesGridUnit[0]), [](int vertexCount, int shaderProgram, glm::mat4 objRBT) {
@@ -29,7 +29,7 @@ GridModel::GridModel() {
 			glDrawArrays(GL_LINES, 2, 2);
 		});
 
-		zline->translate(-50.0f + i, 0, 0);
+		zline->translate(-25.0f + i/2.0f, 0, 0);
 		zline->setupAttribPointer();
 
 		this->addChild(xline);
