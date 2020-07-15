677ae49d94576435002293bdd557fd2b9681abd7
Cedric Martens <cedricmartens98@gmail.com>
2020-07-15 19:11:11 -0400

    Added camera into model

--- Source/includes/Model.hpp
+++ Source/includes/Model.hpp
@@ -6,10 +6,17 @@
 #include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
 // initializing OpenGL and binding inputs
 #include <vector>
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
 
+#ifdef __unix__                    /* __unix__ is usually defined by compilers targeting Unix systems */
+
+#include "includes/Camera.hpp"
+
+
+#elif defined(_WIN32) || defined(WIN32)     /* _Win32 is usually defined by compilers targeting 32 or   64 bit Windows systems */
+
+#include "../Source/includes/Camera.hpp"
+
+#endif
 
 class Model
 {
@@ -23,11 +30,13 @@ public:
 	void(*drawFunction)(int vertexCount, int shaderProgram, glm::mat4 objRBT) = [](int vertexCount, int shaderProgram, glm::mat4 objRBT) {};
 	std::vector<Model*> children;
 	Model* parent = nullptr;
+	Camera* camera = nullptr;
 
 	Model();
 	Model(glm::vec3* vertexArray, int vertexCount, void(*drawFunc)(int vertexCount, int shaderProgram, glm::mat4 objRBT));
 	virtual void setupAttribPointer() = 0;
 	virtual void setShader(int shaderProgram);
+	virtual void setCamera(Camera* camera);
 	virtual void draw();
 	void scale(float x, float y, float z);
 	void rotate(float y, float x, float z, float angle);
