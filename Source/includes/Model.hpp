1bdc1760cc5a990f60358cde55bd10f8351125ae
Laetitia Fesselier <laetitia.fesselier@mail.mcgill.ca>
2020-07-18 03:19:35 -0400

    Ubuntu Compilation fix

--- Source/includes/Model.hpp
+++ Source/includes/Model.hpp
@@ -9,7 +9,7 @@
 
 #ifdef __unix__                    /* __unix__ is usually defined by compilers targeting Unix systems */
 
-#include "includes/Camera.hpp"
+#include "Camera.hpp"
 
 
 #elif defined(_WIN32) || defined(WIN32)     /* _Win32 is usually defined by compilers targeting 32 or   64 bit Windows systems */
