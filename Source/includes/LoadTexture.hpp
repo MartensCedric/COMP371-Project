c923a42caabde789d92c5fa12e787258e755b701
Laetitia Fesselier <laetitia.fesselier@mail.mcgill.ca>
2020-07-22 21:26:50 -0400

    Fix Ubuntu compilation

--- Source/includes/LoadTexture.hpp
+++ Source/includes/LoadTexture.hpp
@@ -6,6 +6,16 @@
 #include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
 #include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
 #include <iostream>
+
+#ifdef __unix__                    /* __unix__ is usually defined by compilers targeting Unix systems */
+
+#define OS_Windows 0
+#include "stb_image.h"
+
+#elif defined(_WIN32) || defined(WIN32)     /* _Win32 is usually defined by compilers targeting 32 or   64 bit Windows systems */
+
+#define OS_Windows 1
 #include "../stb_image.h"
+#endif
 
 GLuint loadTexture(const char *filename);
