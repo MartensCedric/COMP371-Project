67ac3e024e9b78b7fb24aa5c7a7f0a8f1b905b5c
Cedric Martens <cedricmartens98@gmail.com>
2020-07-26 18:36:53 -0400

    Moved light in the air

--- Source/UnitCubeModel.cpp
+++ Source/UnitCubeModel.cpp
@@ -79,9 +79,9 @@ UnitCubeModel::UnitCubeModel() {
             glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &objRBT[0][0]);
             glDrawArrays(GL_TRIANGLES, 0, vertexCount);
 
-			glm::mat4 lightProjection = glm::ortho(-10.f, 10.f, -10.f, 10.f,
-				1.0f, 25.f);         // near and far (near > 0)
-			glm::mat4 lightView = glm::lookAt(glm::vec3(0.0f,5.0f, 6.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0, 1, 0));
+			glm::mat4 lightProjection = glm::ortho(-25.f, 25.f, -25.f, 25.f,
+				1.0f, 50.f);         // near and far (near > 0)
+			glm::mat4 lightView = glm::lookAt(glm::vec3(0.0f,30.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0, 1, 0));
 
 			glm::mat4 transformLightSpace = lightProjection * lightView * objRBT;
 			int transformLightSpaceLocation = glGetUniformLocation(shaderProgram, "transform_in_light_space");
